d32cdf617ec95ea43ad3ec3cb98bbc7e1544e935
adanestrada <adanestrada@users.noreply.github.com>
2025-11-06 07:12:45 -0600

    Update so_host_commands.sh
    add remove container command

--- so_host_commands.sh
+++ so_host_commands.sh
@@ -12,9 +12,14 @@ docker run -d -p 8080:8080 -p 7077:7077 --name spark-master apache/spark:latest 
 #3. Example command to Start a Spark Worker and add it to the cluster
 #Run a Docker container for a Spark worker, linking it to the master. Replace spark-master with the actual name or IP of your Spark master container if it's different.
 docker run -d --name spark-worker-1 --link spark-master:spark-master apache/spark:latest /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077 --cores 1 --memory 4g
+docker run -d --name spark-worker-2 --link spark-master:spark-master apache/spark:latest /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077 --cores 2 --memory 2g
 
 #4. Example command to submit a Spark Application (Pi Montecarlo using 10 partitions):
-docker exec spark-master /opt/spark/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://172.17.0.2:7077 /opt/spark/examples/jars/spark-examples_2.13-4.0.1.jar 10
+docker exec spark-master /opt/spark/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://172.17.0.2:7077 /opt/spark/examples/jars/spark-examples_2.13-4.0.1.jar 10000
+
+#Example command to delete container
+### Remove the Container: docker rm <container_name>
+docker rm spark-worker-1
 
 
 
@@ -26,7 +31,8 @@ docker exec spark-master /opt/spark/bin/spark-submit --class org.apache.spark.ex
 # - Crear un cluster de Spark con un nodo y dos cores con 3 gb de RAM
 # - Correr el ejemplo de Cálculo de Pi usando: 3000 4000 y 5000 particiones
 #
-# Nota, se debe modificar el comando de la creación del worker (Ejecutar este comando una sola vez)
+# Nota, se debe modificar el comando de la creación del worker 
+#(Ejecutar este comando una sola vez)
 
 
 # Se debe modificar el comando de ejecución de una aplicación de Spark en tres ocasiones.
@@ -46,7 +52,7 @@ docker exec spark-master /opt/spark/bin/spark-submit --class org.apache.spark.ex
 # Se debe modificar el comando de ejecución de una aplicación de Spark en tres ocasiones.
 # Crear su pdf de entrega de evidencias con la captura de la pantalla de la Spark UI después de haber ejecutado 3 veces la aplicación de Pi
 # Crear una tabla con los resultados de Pi, error y tiempo de ejecución del cluster por cada ejecución (3)
-# Después de finalizar el Ejercicio 1 y obntener su evidencia, eliminar los contenedores de los nodos worker. (Solo dejar contenedor del nodo Master) Para Ejericio 2
+
 #########################################################
 
 
@@ -60,5 +66,5 @@ docker exec spark-master /opt/spark/bin/spark-submit --class org.apache.spark.ex
 # Se debe modificar el comando de ejecución de una aplicación de Spark en tres ocasiones.
 # Crear su pdf de entrega de evidencias con la captura de la pantalla de la Spark UI después de haber ejecutado 3 veces la aplicación de Pi
 # Crear una tabla con los resultados de Pi, error y tiempo de ejecución del cluster por cada ejecución (3)
-# Después de finalizar el Ejercicio 1 y obntener su evidencia, eliminar los contenedores de los nodos worker. (Solo dejar contenedor del nodo Master) Para Ejericio 2
+
 #########################################################
